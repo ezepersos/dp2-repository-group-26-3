f3e0e26b29708ab94b8da7cf3e43da2a78a2ea2d
Alejandro Baños Gonzalez <alebangon@alum.us.es>
2021-05-04 20:43:00 +0200

    Arreglado authorise de admin

--- src/main/java/acme/features/administrator/spam/AdministratorSpamRepository.java
+++ src/main/java/acme/features/administrator/spam/AdministratorSpamRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.spam.Spam;
+import acme.framework.entities.Administrator;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
@@ -13,6 +14,9 @@ public interface AdministratorSpamRepository extends AbstractRepository {
 
 	@Query("select s from Spam s") 
 	Collection<Spam> searchOne();
+	
+	@Query("select a from Administrator a where a.id=?1")
+	Administrator findAdminById(int id);
 }
 
 	
