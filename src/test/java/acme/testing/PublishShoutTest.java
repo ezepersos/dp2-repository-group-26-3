6ab46b43b230ce727d74c592861b4a550a22bc00
Pablo <pabloguti1006@gmail.com>
2021-05-20 16:48:38 +0200

    PublishingShoutTest

--- src/test/java/acme/testing/PublishShoutTest.java
+++ src/test/java/acme/testing/PublishShoutTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
 
-import acme.framework.testing.AbstractTest;
-
-public class PublishShoutTest extends AbstractTest {
+public class PublishShoutTest extends AcmeTest {
 
 	// Internal state ---------------------------------------------------------
 
@@ -22,9 +20,9 @@ public class PublishShoutTest extends AbstractTest {
 		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
 		super.setAutoPausing(true);
 		this.signIn("administrator", "administrator");
-		super.click(By.linkText("Administrator"));
-		super.submit(By.linkText("Populate DB (initial)"));
+		super.clickOnMenu("Administrator", "Populate DB (initial)");
 		this.signOut();
+		
 	}
 	// Test cases -------------------------------------------------------------
 
@@ -33,12 +31,11 @@ public class PublishShoutTest extends AbstractTest {
 	@Order(10)
 	public void positivePublishShout(final String author, final String text, final String info) {
 		super.navigateHome();
-		super.click(By.linkText("Anonymous"));
-		super.click(By.linkText("Shout!"));
+		super.clickOnMenu("Anonymous", "Shout!");
 		super.fill(By.id("author"), author);
 		super.fill(By.id("text"), text);
 		super.fill(By.id("info"), info);
-		super.submit(By.className("btn-primary"));
+		super.clickOnSubmitButton("Shout!");
 		
 	}
 	
@@ -47,12 +44,13 @@ public class PublishShoutTest extends AbstractTest {
 	@Order(10)
 	public void negativePublishShout(final String author, final String text, final String info) {
 		super.navigateHome();
-		super.click(By.linkText("Anonymous"));
-		super.click(By.linkText("Shout!"));
+		super.clickOnMenu("Anonymous", "Shout!");
 		super.fill(By.id("author"), author);
 		super.fill(By.id("text"), text);
 		super.fill(By.id("info"), info);
-		super.submit(By.className("btn-primary"));
+		super.clickOnSubmitButton("Shout!");
+		super.checkErrorsExist();
+		
 	}
 
 	// Ancillary methods ------------------------------------------------------
@@ -60,29 +58,30 @@ public class PublishShoutTest extends AbstractTest {
 	
 	protected void signIn(final String username, final String password) {
 		super.navigateHome();
-		super.click(By.linkText("Sign in"));
+		super.clickAndGo(By.linkText("Sign in"));
 		super.fill(By.id("username"), username);
 		super.fill(By.id("password"), password);
-		super.click(By.id("remember$proxy"));
-		super.submit(By.className("btn-primary"));
+		super.clickAndGo(By.id("remember$proxy"));
+		super.clickOnSubmitButton("Sign in");
+		
 	}
 
 	protected void signOut() {
 		super.navigateHome();
-		super.submit(By.linkText("Sign out"));
+		super.clickAndGo(By.linkText("Sign out"));
 	}
 
 	protected void signUp(final String username, final String password, final String name, final String surname, final String email) {
 		super.navigateHome();
-		super.click(By.linkText("Sign up"));
+		super.clickAndGo(By.linkText("Sign in"));
 		super.fill(By.id("username"), username);
 		super.fill(By.id("password"), password);
 		super.fill(By.id("confirmation"), password);
 		super.fill(By.id("identity.name"), name);
 		super.fill(By.id("identity.surname"), surname);
 		super.fill(By.id("identity.email"), email);
-		super.click(By.id("accept$proxy"));
-		super.submit(By.className("btn-primary"));
+		super.clickAndGo(By.id("accept$proxy"));
+		super.clickOnSubmitButton("Sign up");
 	}
 
 }
