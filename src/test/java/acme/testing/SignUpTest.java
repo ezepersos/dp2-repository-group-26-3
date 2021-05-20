fedb3ef497c4fea3da9ea15ae6ac4e6d68d6464d
Romanflodriguez <Romanflodriguez13@gmail.com>
2021-05-20 16:24:00 +0200

    UPGRADE PROJECT

--- src/test/java/acme/testing/SignUpTest.java
+++ src/test/java/acme/testing/SignUpTest.java
@@ -14,65 +14,7 @@ package acme.testing;
 
 public class SignUpTest {
 
-public class SignUpTest extends AcmeTest {
-
-	// Internal state ---------------------------------------------------------
-
-	// Lifecycle management ---------------------------------------------------
-
-	@Override
-	@BeforeAll
-	public void beforeAll() {
-		super.beforeAll();
-
-		super.setBaseCamp("http", "localhost", "8080", "/Acme-Planner", "/master/welcome", "?language=en&debug=true");
-		super.setAutoPausing(true);
-
-		this.signIn("administrator", "administrator");
-		super.clickAndGo(By.linkText("Administrator"));
-		super.clickAndGo(By.linkText("Populate DB (initial)"));
-		this.signOut();
-	}
-
-	// Test cases -------------------------------------------------------------
-
-	@ParameterizedTest
-	@CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(1)
-	public void positiveSignUp(final String username, final String password, final String name, final String surname, final String email) {
-		this.signUp(username, password, name, surname, email);
-		this.signIn(username, password);
-		assert super.exists(By.linkText("Account"));
-		this.signOut();
-	}
-
-	// Ancillary methods ------------------------------------------------------
-
-	protected void signIn(final String username, final String password) {
-		super.navigateHome();
-		super.clickAndGo(By.linkText("Sign in"));
-		super.fill(By.id("username"), username);
-		super.fill(By.id("password"), password);
-		super.clickAndGo(By.id("remember$proxy"));
-		super.clickOnSubmitButton("Sign in");
-	}
-
-	protected void signOut() {
-		super.navigateHome();
-		super.clickAndGo(By.linkText("Sign out"));
-	}
-
-	protected void signUp(final String username, final String password, final String name, final String surname, final String email) {
-		super.navigateHome();
-		super.clickAndGo(By.linkText("Sign up"));
-		super.fill(By.id("username"), username);
-		super.fill(By.id("password"), password);
-		super.fill(By.id("confirmation"), password);
-		super.fill(By.id("identity.name"), name);
-		super.fill(By.id("identity.surname"), surname);
-		super.fill(By.id("identity.email"), email);
-		super.clickAndGo(By.id("accept$proxy"));
-		super.clickOnSubmitButton("Sign up");
-	}
-
+	// This is a placeholder where you can introduce your own sign-up test
+  	// Note that it depends on your project-specific test class.
+	
 }
