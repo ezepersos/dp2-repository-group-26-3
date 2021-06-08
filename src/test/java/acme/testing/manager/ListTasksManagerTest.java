f677bf672020dd5ba95f78185fedd2e4d101e208
julibamon <julibamon@alum.us.es>
2021-06-08 04:01:11 +0200

    arreglados tests manager

--- src/test/java/acme/testing/manager/ListTasksManagerTest.java
+++ src/test/java/acme/testing/manager/ListTasksManagerTest.java
@@ -63,18 +63,7 @@ public class ListTasksManagerTest extends AcmePlannerTest {
 		super.clickAndGo(By.linkText("Sign out"));
 	}
 
-	protected void signUp(final String username, final String password, final String name, final String surname, final String email) {
-		super.navigateHome();
-		super.clickAndGo(By.linkText("Sign up"));
-		super.fill(By.id("username"), username);
-		super.fill(By.id("password"), password);
-		super.fill(By.id("confirmation"), password);
-		super.fill(By.id("identity.name"), name);
-		super.fill(By.id("identity.surname"), surname);
-		super.fill(By.id("identity.email"), email);
-		super.clickAndGo(By.id("accept$proxy"));
-		super.clickOnSubmitButton("Sign up");
-	}
+
 
 }
 
